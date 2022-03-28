4b501420a03b2bd5c53867113da36f6164503cc3
takuma123-type <72244249+takuma123-type@users.noreply.github.com>
2022-03-29 04:13:35 +0900

    完成

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -16,5 +16,20 @@ use App\Http\Controllers\BlogController;
 //  ブログ一覧画面を表示
 Route::get('/', [BlogController::class, 'showList'])->name('blogs');
 
+//登録画面を表示
+Route::get('/blog/create/', [BlogController::class, 'showCreate'])->name('create');
+
+//ブログ登録
+Route::post('/blog/store/', [BlogController::class, 'showStore'])->name('store');
+
 //ブログ詳細画面を表示
 Route::get('/blog/{id}', [BlogController::class, 'showDetail'])->name('show');
+
+//ブログ編集画面を表示
+Route::get('/blog/edit/{id}', [BlogController::class, 'showEdit'])->name('edit');
+
+
+Route::post('/blog/update/', [BlogController::class, 'showUpdate'])->name('update');
+
+Route::post('/blog/delete/{id}', [BlogController::class, 'showDelete'])->name('delete');
+
